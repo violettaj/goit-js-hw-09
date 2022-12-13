56bdf1f187818302ca58928d315bbf6898f5f557
Wiola Janczura <wiola.janczura@wp.pl>
2022-12-13 14:52:24 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  const startBtn = document.querySelector("button[data-start]")
+const stopBtn = document.querySelector("button[data-stop]")
 
+let interval = null;
+
+const stopInterval = () => {
+    if (interval) clearInterval(interval);
+    stopBtn.setAttribute("disabled", "");
+    startBtn.removeAttribute("disabled", "");
+}
+
+const startInterval = () => {
+    if (interval) clearInterval(interval);
+  startBtn.setAttribute("disabled", "");
+  stopBtn.removeAttribute("disabled", "");
+
+  interval = setInterval(() => {
+    
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  }, 1000);
+};
+
+
+
+startBtn.addEventListener("click", startInterval);
+stopBtn.addEventListener("click", stopInterval);
