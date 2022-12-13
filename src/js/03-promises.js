c257071c16ab152d1de9452c4cec21681e2355fa
Wiola Janczura <wiola.janczura@wp.pl>
2022-12-14 00:28:48 +0100

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", submitEventHandler);
+
+const firstDelay = document.querySelector('input[name="delay"]');
+const stepDelay = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
+function submitEventHandler(event) {
+  event.preventDefault();
+
+  const delay = Number.parseInt(firstDelay.value);
+  const step = Number.parseInt(stepDelay.value);
+
+  for (let i = 0; i < amount.value; i++) {
+    createPromise(i + 1, delay + step * i)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay}) 
   } else {
-    // Reject
+    reject({ position, delay}) 
   }
+},delay);
+})
 }
